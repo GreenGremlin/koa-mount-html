5895e55b5d67fd67ad74520606229734f7b5fb62
Jonathan Felchlin <jonathan@xgecko.com>
2015-12-23 02:21:59 -0800

    Got it working for index rewrite

--- src/index.js
+++ src/index.js
@@ -13,19 +13,19 @@ const CONTENT_TYPE_HTML = 'text/html';
 // }
 
 
-// function evaluateRewriteRule(parsedUrl, match, rule) {
-//   if (typeof rule === 'string') {
-//     return rule;
-//   }
-//   else if (typeof rule !== 'function') {
-//     throw new Error('Rewrite rule can only be of type string of function.');
-//   }
+function evaluateRewriteRule(parsedUrl, match, rule) {
+  if (typeof rule === 'string') {
+    return rule;
+  }
+  else if (typeof rule !== 'function') {
+    throw new Error('Rewrite rule can only be of type string of function.');
+  }
 
-//   return rule({
-//     parsedUrl: parsedUrl,
-//     match: match
-//   });
-// }
+  return rule({
+    parsedUrl: parsedUrl,
+    match: match
+  });
+}
 
 function acceptsHtml(header) {
   return header.indexOf('text/html') !== -1 || header.indexOf('*/*') !== -1;
@@ -41,12 +41,13 @@ function acceptsHtml(header) {
 //   return () => {};
 // }
 
-exports = module.exports = function historyApiFallback(app, _options) {
-  const options = _options || {};
+exports = module.exports = function historyApiFallback(options, app) {
+  // const app = _app ? _app : _options;
+  // const options = _app ? _options : {};
   // const logger = getLogger(options);
 
   // compose
-  const downstream = app.middleware
+  const downstream = app && app.middleware
     ? compose(app.middleware)
     : app;
 
@@ -77,25 +78,11 @@ exports = module.exports = function historyApiFallback(app, _options) {
   return function* (upstream) {
     // var headers = req.headers;
     const accepts = this.accepts().toString();
-    // let reason = '';
-    let parsedUrl;
-    // let rewriteTarget;
-    // let rewrites;
-    // let rewritten;
-
-    // if (this.request.method !== 'GET') {
-    //   reason = 'because the method is not GET.';
-    // }
-    // else if (!accepts || typeof accepts !== 'string') {
-    //   reason = 'because the client did not send an HTTP accept header.';
-    // }
-    // else if (accepts.indexOf('application/json') === 0) {
-    //   reason = 'because the client prefers JSON.';
-    // }
-    // else if (!acceptsHtml(accepts)) {
-    //   reason = 'because the client does not accept HTML.';
-    // }
-    // else {
+    const originalUrl = this.request.url;
+    var parsedUrl;
+    var rewriteTarget;
+    var rewrites;
+    var rewritten;
 
     if (this.request.method === 'GET'
         && accepts && typeof accepts === 'string'
@@ -103,29 +90,31 @@ exports = module.exports = function historyApiFallback(app, _options) {
         && acceptsHtml(accepts)) {
 
       parsedUrl = url.parse(this.request.url);
-      // rewrites = options.rewrites || [];
+      rewrites = options.rewrites || [];
 
-      // rewritten = rewrites.some(rewrite => {
-      //   const match = parsedUrl.pathname.match(rewrite.from);
+      rewritten = rewrites.some(rewrite => {
+        const match = parsedUrl.pathname.match(rewrite.from);
 
-      //   if (match !== null) {
-      //     yield* downstream.call(this, function *(){
-      //       yield* upstream;
-      //     }.call(this));
-      //   }
-      // });
-      // if (rewritten) {
-      //   yield *upstream;
-      // }
+        if (match !== null) {
+          rewriteTarget = evaluateRewriteRule(parsedUrl, match, rewrite.to);
+          return true;
+        }
+      });
+      if (rewritten || parsedUrl.pathname.indexOf('.') === -1) {
+        console.info(`this.request.url: ${this.request.url}`);
+        this.request.url = rewritten ? rewriteTarget : (options.index || '/index.html');
+        console.info(`this.request.url: ${this.request.url}`);
 
-      if (parsedUrl.pathname.indexOf('.') === -1) {
-        yield* downstream.call(this, function *(){
+        if (downstream) {
+          yield* downstream.call(this, function *(){
+            this.request.url = originalUrl;
+            yield* upstream;
+          }.call(this));
+        }
+        else {
           yield* upstream;
-        }.call(this));
+        }
       }
-      // else {
-      //   reason = 'because the path includes a dot (.) character.';
-      // }
     }
 
     yield *upstream;
